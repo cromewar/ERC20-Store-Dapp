d4d5150a079776a250a461272621007983084ce7
cromewar <vasiliy@cromewar.com>
2022-02-22 12:54:56 -0500

    project creation

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import logo from "../logo.png";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <div>Hello Crome</div>;
+  }
+}
+
+export default App;
